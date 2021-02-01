b981269008e1dff3db77c775b1f7d8ca2f6de09e
Bobert Nieto <discordemerde@aol.com>
2021-02-01 15:46:23 +0100

    ajoute le bouton

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Mes selecteurs
-// var mybutton = document.getElementById("mybutton");
+var mybutton = document.getElementById("mybutton");
 var quote = document.getElementById("quote");
 //mes variables
 
@@ -7,10 +7,16 @@ var citation = "";
 
 //mes evevenements
 
-// mybutton.addEventListener('click' ,function(){
+
 window.onload = async() => {  
   await fetch("https://api.kanye.rest")
   .then(reponse => reponse.json())
   .then(data => citation = data.quote);
     quote.textContent = citation;
   }
+mybutton.addEventListener('click' , async() =>{
+    await fetch("https://api.kanye.rest")
+    .then(reponse => reponse.json())
+    .then(data => citation =data.quote);
+    quote.textContent =citation;
+})
